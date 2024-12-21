56addd35f078c7821e37ccb989c6f33812f9091b
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2024-12-21 15:37:57 +0200

    feature: use DB - refactor GET /games endpoint to use DB context instead of hard coded data list

--- Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
+++ Backend/src/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
@@ -1,4 +1,5 @@
 using GameStore.Api.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Features.Games.GetGames;
 
@@ -7,12 +8,16 @@ public static class GetGamesEndpoint
 	public static void MagGetGames(this IEndpointRouteBuilder app)
 	{
 		// GET /games
-		app.MapGet("/", ( GameStoreData data) => data.GetGames().Select(game => new GameSummaryDto(
-			game.Id,
-			game.Name,
-			game.Genre!.Name,
-			game.Price,
-			game.ReleaseDate
-		)));
+		app.MapGet("/", ( GameStoreDbContext context) => 
+			context.Games
+				.Include(game => game.Genre)
+				.Select(game => new GameSummaryDto(
+					game.Id,
+					game.Name,
+					game.Genre!.Name,
+					game.Price,
+					game.ReleaseDate
+				))
+				.AsNoTracking());
 	}
 }
